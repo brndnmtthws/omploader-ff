bc8218c3bf04acb3d9fb1b9a79cf85d28375d7b2
Jeff Glover <jeff.web@sigmatheory.com>
2007-09-06 08:48:06 +0000

    Attempted to allow for omploading from a local file. Within ompLoadURI comment out the alert and gBrowser.loadURI and uncomment "this.ompLoadFile(uri);" to try this code. Using Live HTTP Headers as far as I can tell everything appears to be identical to a normal upload. The only difference is the Content-Length is slightly less. This could be a slight difference in headers or the root of the problem.
    git-svn-id: http://svn.omploader.org/trunk/firefox-extension@200 f3e9fb10-4bd1-4be2-8a77-beb9a613051d

--- content/overlay.js
+++ content/overlay.js
@@ -13,11 +13,15 @@ var omploader = {
 	onLoad: function() {
 		// initialization code
 		this.postVars = new Array();
-		this.postVars['url'] = "url1=";
-		this.postVars['file'] = "file1="; // one day this may useful
+		this.postVars['url'] = "url1";
+		this.postVars['file'] = "file1"; // one day this may useful
 
-		this.ompURL = "http://omploader.org/upload";
-// 		this.ompURL = "http://test.peemail.org/upload"; // for testing
+// 		this.ompHost = "http://test.peemail.org";  // for testing
+		this.ompHost = "http://omploader.org";
+
+		this.ompURL = this.ompHost + "/upload";
+		
+		this.ompFileURL = this.ompHost + "/file";
 
 		this.Cc = Components.classes;
 		this.Ci = Components.interfaces;
@@ -26,6 +30,8 @@ var omploader = {
 
 		this.initialized = true;
 		this.strings = document.getElementById("omploader-strings");
+		
+		this.multipart_boundary = "-----------------------------" + new Date().getTime();
 	},
 
 	showContextMenu: function(event) {
@@ -91,11 +97,16 @@ var omploader = {
 	},
 
 	ompLoadURI: function(uri) {
-		if(uri.scheme == "file")
+		if(uri.scheme == "file") {
 			alert("Ompload does not yet support omploading local files.\n\nUser contributions are welcome. If you are interested in helping omploader, http://developer.mozilla.org/en/docs/XMLHttpRequest may be a good start to omplement this feature. In addition the ImageBot extension (http://pimpsofpain.com/imagebot.html) also uses similar functionality.");
+			gBrowser.loadURI(this.ompFileURL, uri, null, null);
+
+// 			this.ompLoadFile(uri);
+			
+		}
 
 		else {
-			var dataString = this.postVars['url'] + uri.spec;
+			var dataString = this.postVars['url'] + "=" + uri.spec;
 		
 			// POST method requests must wrap the encoded text in a MIME
 			// stream
@@ -114,6 +125,128 @@ var omploader = {
 		
 			gBrowser.addTab(this.ompURL, uri, null, postData);
 		}
+	},
+	
+	ompLoadFile: function(uri) {
+		var nsIFile_file = this.Cc["@mozilla.org/file/local;1"].createInstance(this.Ci.nsILocalFile);
+		nsIFile_file.initWithPath(uri.path);
+		
+		var nsIFileInputStream = this.Cc["@mozilla.org/network/file-input-stream;1"].createInstance(this.Ci.nsIFileInputStream);
+		
+// 		nsIFileInputStream.init(nsIFile_file, 1, 1,
+// 			this.Ci.nsIFileInputStream.CLOSE_ON_EOF);
+			
+		nsIFileInputStream.init(nsIFile_file, 0x01, 00004, null);
+		
+		var nsIBufferedInputStream = this.Cc["@mozilla.org/network/buffered-input-stream;1"].createInstance(this.Ci.nsIBufferedInputStream);
+		nsIBufferedInputStream.init(nsIFileInputStream, 1024);
+		
+		var nsIStringInputStream_start = this.Cc["@mozilla.org/io/string-input-stream;1"].createInstance(this.Ci.nsIStringInputStream);
+		nsIStringInputStream_start.setData(this.getStreamData(nsIFile_file), -1);
+		
+		var nsIStringInputStream_end = this.Cc["@mozilla.org/io/string-input-stream;1"].createInstance(this.Ci.nsIStringInputStream);
+		nsIStringInputStream_end.setData("\n" + this.multipart_boundary + "--\n", -1);
+
+		var nsIMultiplexInputStream = this.Cc["@mozilla.org/io/multiplex-input-stream;1"].createInstance(this.Ci.nsIMultiplexInputStream);
+		
+		nsIMultiplexInputStream.appendStream(nsIStringInputStream_start);
+		nsIMultiplexInputStream.appendStream(nsIBufferedInputStream);
+		nsIMultiplexInputStream.appendStream(nsIStringInputStream_end);
+		
+		var postData = this.Cc["@mozilla.org/network/mime-input-stream;1"].
+			createInstance(this.Ci.nsIMIMEInputStream);
+		postData.addHeader("Content-Type", "multipart/form-data; boundary=" + this.multipart_boundary);
+		postData.addContentLength = true;
+		postData.setData(nsIMultiplexInputStream);
+		
+// 		var xmlr = new XMLHttpRequest();
+// 		xmlr.open("POST", this.ompURL, false);
+// 		xmlr.setRequestHeader("Content-Type", "multipart/form-data; boundary=" + this.multipart_boundary);
+// 		xmlr.setRequestHeader("Content-Length", nsIMultiplexInputStream.available() - 2 );
+// 		xmlr.setRequestHeader("Content-Length", nsIMultiplexInputStream.available());
+		
+// 		xmlr.send(postData);
+		
+// 		if ( xmlr.status != "200" )
+// 			alert("Error: " + xmlr.status + "\nMessage: " + xmlr.responseText);
+// 		else
+// 			alert(xmlr.responseText);
+		
+// 		this.alertInputStream(postData);
+
+		var refuri = this.ioService.newURI(this.ompURL, null, null);
+
+		gBrowser.addTab(this.ompURL, refuri, null, postData);
+	},
+	
+	getStreamData: function(nsIFile_file) {
+		var file_name = nsIFile_file.leafName;
+		
+		var mimeType = "application/octet-stream";
+		
+		try {
+			var mimeService = this.Cc["@mozilla.org/uriloader/external-helper-app-service;1"].getService(this.Ci.nsIMIMEService);
+			
+			mimeType = mimeService.getTypeFromFile(nsIFile_file);
+		} catch (e) {
+			mimeType = "application/octet-stream";
+		}
+	
+// 		var result = this.multipart_boundary +
+// 			"\r\nContent-Disposition: form-data; name=\"action\"\r\n\r\nupload\r\n" +
+// 			this.multipart_boundary +
+// 			"\r\nContent-Disposition: form-data; name=\"" + 
+// 			this.postVars['file'] +
+// 			"\"; filename=\"" + file_name + "\"\r\n" +
+// 			"Content-Type: " + mimeType + "\r\n\r\n";
+
+		var result = this.multipart_boundary +
+// 			"\nContent-Disposition: form-data; name=\"action\"\n\nupload\n" +
+// 			this.multipart_boundary +
+			"\nContent-Disposition: form-data; name=\"" + 
+			this.postVars['file'] +
+			"\"; filename=\"" + file_name + "\"\n" +
+			"Content-Type: " + mimeType + "\n\n";
+		
+// 		alert(result);
+		
+		return result;
+	},
+	
+	// display a stream in an alert. use for debugging
+	alertInputStream: function (nsiInputStream) {
+		var charset = /* Need to find out what the character encoding is. Using UTF-8 for this example: */ "UTF-8";
+		const replacementChar = this.Ci.nsIConverterInputStream.DEFAULT_REPLACEMENT_CHARACTER;
+		var is = Components.classes["@mozilla.org/intl/converter-input-stream;1"]
+			.createInstance(Components.interfaces.nsIConverterInputStream);
+		
+		is.init(nsiInputStream, charset, 1024, 0xFFFD);
+		
+		
+		var streamEnd = "";
+		var streamBegin = "";
+		var count = 0;
+		
+		if (is instanceof Components.interfaces.nsIUnicharLineInputStream) {
+			var line = {};
+			var cont;
+			do {
+				cont = is.readLine(line);
+
+				// Now you can do something with line.value
+				streamEnd = line.value;
+				if (count < 9)
+					streamBegin += line.value + "\n";
+					
+				count++;
+				
+			} while (cont);
+		}
+		
+		is.close();
+		
+		alert(streamBegin);
+// 		alert(streamEnd);
 	}
 
 };
