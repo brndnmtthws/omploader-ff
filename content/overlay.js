c4278a5a98eb027b6a226823e7d71457a2d8726b
Jeff Glover <jeff.web@sigmatheory.com>
2007-09-09 01:38:30 +0000

    Assuming no bugs are found, this code should be ok for a major release - Hides the submenu items if the protocol is "file". If no items are visible, then disables the menu - Added "this.noSupportFile" variable to allow for testing
    git-svn-id: http://svn.omploader.org/trunk/firefox-extension@203 f3e9fb10-4bd1-4be2-8a77-beb9a613051d

--- content/overlay.js
+++ content/overlay.js
@@ -14,7 +14,7 @@ var omploader = {
 		// initialization code
 		this.postVars = new Array();
 		this.postVars['url'] = "url1";
-		this.postVars['file'] = "file1"; // one day this may useful
+		this.postVars['file'] = "file1";
 
 // 		this.ompHost = "http://test.peemail.org";  // for testing
 		this.ompHost = "http://omploader.org";
@@ -31,19 +31,29 @@ var omploader = {
 		this.initialized = true;
 		this.strings = document.getElementById("omploader-strings");
 		
+		// set to false to support the file:// protocol
+		// this doesn't work, only use if for development
+		this.noSupportFile = true;
+
 		this.multipart_boundary = "-----------------------------" + new Date().getTime();
+
+		this.contextMenu = document.getElementById("contentAreaContextMenu");
+		this.contextMenu.addEventListener("popupshowing", this.showContextMenu, false);
 	},
 
 	showContextMenu: function(event) {
 	// show or hide the menuitem based on what the context menu is on
 	// see http://kb.mozillazine.org/Adding_items_to_menus
-
+		var menuMain = document.getElementById("context-omploader");
 		var menuImage = document.getElementById("omploader-image-menuitem");
 		var menuBGImage = document.getElementById("omploader-bgimage-menuitem");
 		var menuLink = document.getElementById("omploader-link-menuitem");
 		var menuPage = document.getElementById("omploader-page-menuitem");
+
+		var numVisibleMenuItems = 0;
 	
-		if(gContextMenu.onImage) {
+		if(gContextMenu.onImage && omploader.checkSupported(gContextMenu.target.currentURI.scheme)) {
+			numVisibleMenuItems++;
 			menuImage.hidden = false;
 			menuImage.setAttribute("tooltiptext", gContextMenu.target.currentURI.spec);
 			menuImage.setAttribute("oncommand", "omploader.onContextMenuItemCommand(gContextMenu.target.currentURI)");
@@ -52,14 +62,21 @@ var omploader = {
 			menuImage.hidden = true;
 	
 		if(gContextMenu.hasBGImage) {
-			menuBGImage.hidden = false;
-			menuBGImage.setAttribute("tooltiptext", gContextMenu.bgImageURL);
-			menuBGImage.setAttribute("oncommand", "omploader.onMenuItemURLCommand(gContextMenu.bgImageURL)");
+			var uri = omploader.URLtoURI(gContextMenu.bgImageURL);
+			if (omploader.checkSupported(uri.scheme))
+				menuBGImage.hidden = true;
+			else {
+				numVisibleMenuItems++;
+				menuBGImage.hidden = false;
+				menuBGImage.setAttribute("tooltiptext", gContextMenu.bgImageURL);
+				menuBGImage.setAttribute("oncommand", "omploader.onMenuItemURLCommand(gContextMenu.bgImageURL)");
+			}
 		}
 		else
 			menuBGImage.hidden = true;
 	
-		if(gContextMenu.onLink) {
+		if(gContextMenu.onLink && omploader.checkSupported(gContextMenu.linkURI.scheme)) {
+			numVisibleMenuItems++;
 			menuLink.hidden = false;
 			menuLink.setAttribute("tooltiptext", gContextMenu.linkURI.spec);
 			menuLink.setAttribute("oncommand", "omploader.onContextMenuItemCommand(gContextMenu.linkURI)");
@@ -67,7 +84,30 @@ var omploader = {
 		else
 			menuLink.hidden = true;
 	
-		menuPage.setAttribute("tooltiptext", content.document.location.href);
+		var pageuri = omploader.URLtoURI(content.document.location.href);
+		if(omploader.checkSupported(pageuri.scheme)) {
+			numVisibleMenuItems++;
+			menuPage.hidden = false;
+			menuPage.setAttribute("tooltiptext", pageuri.spec);
+		}
+		else
+			menuPage.hidden = true;
+
+		if (numVisibleMenuItems > 0) {
+			menuMain.setAttribute("tooltiptext", omploader.strings.getString("omploaderLabel"));
+			menuMain.setAttribute("disabled", "false");
+		}
+		else {
+			menuMain.setAttribute("tooltiptext", omploader.strings.getString("disableFile"));
+			menuMain.setAttribute("disabled", "true");
+		}
+	},
+
+	checkSupported: function(string) {
+		if (string == "file" && this.noSupportFile)
+			return false;
+		else
+			return true;
 	},
 
 	onContextMenuItemCommand: function(uri) {
@@ -76,33 +116,48 @@ var omploader = {
 	},
 
 	onMenuItemURLCommand: function(url) {
-		this.onContextMenuItemCommand(this.ioService.newURI(url, null, null));
+		this.ompLoadURI(this.URLtoURI(url));
 	},
 
 	onMenuItemCommand: function(e) {
 		var uri;
 
-		uri = this.ioService.newURI(content.document.location.href, null, null);
+		uri = this.URLtoURI(content.document.location.href);
 
-		this.ompLoadURI(uri);
+		if(omploader.checkSupported(uri.scheme)) {
+			this.ompLoadURI(uri);
+		}
 	},
 
 	onToolbarButtonCommand: function(e) {
 		this.onMenuItemCommand(e);
 	},
 
+	URLtoURI: function(url) {
+		return this.ioService.newURI(url, null, null);
+	},
+
 	updateToolsPopup: function(e) {
 		var menuTools = document.getElementById("omploader-tools");
-		menuTools.setAttribute("tooltiptext", content.document.location.href);
+		var pageuri = this.URLtoURI(content.document.location.href);
+
+		if (this.checkSupported(pageuri.scheme)) {
+			menuTools.setAttribute("tooltiptext", pageuri.spec);
+			menuTools.setAttribute("disabled", "false");
+		}
+		else {
+			menuTools.setAttribute("tooltiptext", this.strings.getString("disableFile"));
+			menuTools.setAttribute("disabled", "true");
+		}
 	},
 
 	ompLoadURI: function(uri) {
-		if(uri.scheme == "file") {
+		if(uri.scheme == "file" && !this.checkSupported(uri.scheme)) {
 			alert("Ompload does not yet support omploading local files.\n\nUser contributions are welcome. If you are interested in helping omploader, http://developer.mozilla.org/en/docs/XMLHttpRequest may be a good start to omplement this feature. In addition the ImageBot extension (http://pimpsofpain.com/imagebot.html) also uses similar functionality.");
-			gBrowser.loadURI(this.ompFileURL, uri, null, null);
+		}
 
-// 			this.ompLoadFile(uri);
-			
+		else if(uri.scheme == "file" && this.checkSupported(uri.scheme)) {
+			this.ompLoadFile(uri);
 		}
 
 		else {
@@ -128,31 +183,38 @@ var omploader = {
 	},
 	
 	ompLoadFile: function(uri) {
+		// first create the file for use with the input stream
 		var nsIFile_file = this.Cc["@mozilla.org/file/local;1"].createInstance(this.Ci.nsILocalFile);
 		nsIFile_file.initWithPath(uri.path);
 		
+		// this opens the file
 		var nsIFileInputStream = this.Cc["@mozilla.org/network/file-input-stream;1"].createInstance(this.Ci.nsIFileInputStream);
 		
 // 		nsIFileInputStream.init(nsIFile_file, 1, 1,
 // 			this.Ci.nsIFileInputStream.CLOSE_ON_EOF);
 			
-		nsIFileInputStream.init(nsIFile_file, 0x01, 00004, null);
+		nsIFileInputStream.init(nsIFile_file, 0x01, 00004, null); // i don't know what's allowed for these flags
 		
+		// this buffers that input
 		var nsIBufferedInputStream = this.Cc["@mozilla.org/network/buffered-input-stream;1"].createInstance(this.Ci.nsIBufferedInputStream);
 		nsIBufferedInputStream.init(nsIFileInputStream, 1024);
 		
+		// sets up the file meta data, not the actual file contents
 		var nsIStringInputStream_start = this.Cc["@mozilla.org/io/string-input-stream;1"].createInstance(this.Ci.nsIStringInputStream);
 		nsIStringInputStream_start.setData(this.getStreamData(nsIFile_file), -1);
 		
+		// end of data
 		var nsIStringInputStream_end = this.Cc["@mozilla.org/io/string-input-stream;1"].createInstance(this.Ci.nsIStringInputStream);
 		nsIStringInputStream_end.setData("\n" + this.multipart_boundary + "--\n", -1);
 
 		var nsIMultiplexInputStream = this.Cc["@mozilla.org/io/multiplex-input-stream;1"].createInstance(this.Ci.nsIMultiplexInputStream);
 		
+		// multiplex everything together
 		nsIMultiplexInputStream.appendStream(nsIStringInputStream_start);
 		nsIMultiplexInputStream.appendStream(nsIBufferedInputStream);
 		nsIMultiplexInputStream.appendStream(nsIStringInputStream_end);
 		
+		// encase all the data so content lengh will be calculated
 		var postData = this.Cc["@mozilla.org/network/mime-input-stream;1"].
 			createInstance(this.Ci.nsIMIMEInputStream);
 		postData.addHeader("Content-Type", "multipart/form-data; boundary=" + this.multipart_boundary);
@@ -174,9 +236,9 @@ var omploader = {
 		
 // 		this.alertInputStream(postData);
 
-		var refuri = this.ioService.newURI(this.ompURL, null, null);
+		var refuri = this.ioService.newURI(this.ompFileURL, null, null);
 
-		gBrowser.addTab(this.ompURL, refuri, null, postData);
+		gBrowser.addTab(this.ompFileURL, refuri, null, postData);
 	},
 	
 	getStreamData: function(nsIFile_file) {
@@ -191,6 +253,8 @@ var omploader = {
 		} catch (e) {
 			mimeType = "application/octet-stream";
 		}
+
+		// not sure if \r\n or \n is proper... gay
 	
 // 		var result = this.multipart_boundary +
 // 			"\r\nContent-Disposition: form-data; name=\"action\"\r\n\r\nupload\r\n" +
